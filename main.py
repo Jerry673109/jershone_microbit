222d264892bee2d99afb0a01a0b04773eead65eb
Jerry673109 <jershone.terin@gmail.com>
2025-05-23 21:29:05 +0200

    after karate

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import randint
+
+number = 0
+number1 = 0
+number11 = 0
+number12 = 0
+opr: List[str] = []
+operatorCounter = 0
+operatorValue = ""
+Stage = 0
+mnmnumber = 0
+messages=["Something magical is happening today",
+"You are a strong human",
+"God loves you"]
+aInUse=0
+bInUse=0
+oprFilled=0
+num1Filled=0
+num2Filled=0
+
 def on_button_pressed_a():
+  print("Starting a3:"+mnmnumber)
+  if mnmnumber == 3:
     global number, number1
     number += 1
     basic.show_number(number)
@@ -6,22 +28,65 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global number11, number12
-    number11 += 1
-    basic.show_number(number11)
-    number12 = number11
+  global mnmnumber,oprFilled,num1Filled,num2Filled
+  print("::"+mnmnumber+"::"+oprFilled+"::"+num1Filled+"::"+num2Filled)
+  if mnmnumber == 3 and oprFilled ==0:
+    oprFilled=1
+    operatorCounter=0
+  if mnmnumber == 3 and oprFilled ==1 and num1Filled ==0:
+    num1Filled = 1
+    number=0
+  if mnmnumber ==3 and oprFilled ==1 and num1Filled ==1 and num2Filled==0:
+    num2Filled = 1
+    number = 0      
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
+  print("B on_logo_pressed")  
+  if mnmnumber == 3 and oprFilled == 0 :  
     global opr, operatorValue, operatorCounter, Stage
     opr = ["+", "-", ":", "x"]
-    basic.show_string("" + opr[operatorCounter])
+    basic.show_string("" + (opr[operatorCounter]))
     operatorValue = opr[operatorCounter]
-    operatorCounter += 1
+    if operatorCounter>=3: 
+        operatorCounter=0 
+    else :
+       operatorCounter += 1
     Stage = 1
+  if oprFilled ==1 and mnmnumber == 3 and num1Filled ==0:
+      global number, number11
+      number+=1
+      basic.show_number(number)
+      number11=number
+  if oprFilled ==1 and num1Filled == 1 and mnmnumber ==3 and num2Filled ==0:
+      global number12,number
+      number +=1
+      basic.show_number(number)
+      number12=number
+  if oprFilled == 1 and num1Filled == 1 and num2Filled == 1 and mnmnumber == 3:
+      if operatorValue == "x":
+        ans = number11*number12
+      elif operatorValue == ":":
+        ans = number11/number12
+      elif operatorValue == "-":
+        ans = number11-number12
+      elif operatorValue == "+":
+        ans = number11+number12
+      else:
+          basic.show_string("Error")
+      basic.show_number(ans)  
+  if mnmnumber == 2:
+    index = randint(0,3);
+    print("Index is :"+index)
+    basic.show_string(messages[index])
+    Stage = 1
+  if mnmnumber == 1:
+    ans = randint(0, 10)
+
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_shake():
+  if mnmnumber == 3: 
     if operatorValue == "x":
         basic.show_number(number1 * number12)
     elif operatorValue == ":":
@@ -35,7 +100,9 @@ def on_gesture_shake():
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def on_logo_pressed():
+    print("Starting")
     global mnmnumber
+    print(mnmnumber)
     if mnmnumber < 3:
         mnmnumber += 1
     else:
@@ -47,16 +114,13 @@ def on_logo_pressed():
     elif mnmnumber == 3:
         basic.show_string("Ca")
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
-
-mnmnumber = 0
-Stage = 0
-operatorValue = ""
-operatorCounter = 0
-opr: List[str] = []
-number12 = 0
-number11 = 0
-number1 = 0
-number = 0
-basic.show_string("Hi")
-basic.show_string("Menu")
-mnm = ["CA,G,C"]
+   
+def on_button_pressed_a3():
+    print("Starting a2 :"+mnmnumber)
+    if mnmnumber ==1:
+        basic.show_string("Game")
+    if mnmnumber == 2:
+       basic.show_string("Chat")
+    if mnmnumber == 3:
+       basic.show_string("Calculator")   
+input.on_button_pressed(Button.A, on_button_pressed_a3)
--- main.ts
+++ main.ts
@@ -1,39 +1,130 @@
+let number = 0
+let number1 = 0
+let number11 = 0
+let number12 = 0
+let opr : string[] = []
+let operatorCounter = 0
+let operatorValue = ""
+let Stage = 0
+let mnmnumber = 0
+let messages = ["Something magical is happening today", "You are a strong human", "God loves you"]
+let aInUse = 0
+let bInUse = 0
+let oprFilled = 0
+let num1Filled = 0
+let num2Filled = 0
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    console.log("Starting a3:" + mnmnumber)
+    if (mnmnumber == 3) {
+        
+        number += 1
+        basic.showNumber(number)
+        number1 = number
+    }
     
-    number += 1
-    basic.showNumber(number)
-    number1 = number
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let operatorCounter: number;
+    let number: number;
     
-    number11 += 1
-    basic.showNumber(number11)
-    number12 = number11
+    console.log("::" + mnmnumber + "::" + oprFilled + "::" + num1Filled + "::" + num2Filled)
+    if (mnmnumber == 3 && oprFilled == 0) {
+        oprFilled = 1
+        operatorCounter = 0
+    }
+    
+    if (mnmnumber == 3 && oprFilled == 1 && num1Filled == 0) {
+        num1Filled = 1
+        number = 0
+    }
+    
+    if (mnmnumber == 3 && oprFilled == 1 && num1Filled == 1 && num2Filled == 0) {
+        num2Filled = 1
+        number = 0
+    }
+    
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let ans: number;
+    let index: number;
+    console.log("B on_logo_pressed")
+    if (mnmnumber == 3 && oprFilled == 0) {
+        
+        opr = ["+", "-", ":", "x"]
+        basic.showString("" + opr[operatorCounter])
+        operatorValue = opr[operatorCounter]
+        if (operatorCounter >= 3) {
+            operatorCounter = 0
+        } else {
+            operatorCounter += 1
+        }
+        
+        Stage = 1
+    }
     
-    opr = ["+", "-", ":", "x"]
-    basic.showString("" + opr[operatorCounter])
-    operatorValue = opr[operatorCounter]
-    operatorCounter += 1
-    Stage = 1
+    if (oprFilled == 1 && mnmnumber == 3 && num1Filled == 0) {
+        
+        number += 1
+        basic.showNumber(number)
+        number11 = number
+    }
+    
+    if (oprFilled == 1 && num1Filled == 1 && mnmnumber == 3 && num2Filled == 0) {
+        
+        number += 1
+        basic.showNumber(number)
+        number12 = number
+    }
+    
+    if (oprFilled == 1 && num1Filled == 1 && num2Filled == 1 && mnmnumber == 3) {
+        if (operatorValue == "x") {
+            ans = number11 * number12
+        } else if (operatorValue == ":") {
+            ans = number11 / number12
+        } else if (operatorValue == "-") {
+            ans = number11 - number12
+        } else if (operatorValue == "+") {
+            ans = number11 + number12
+        } else {
+            basic.showString("Error")
+        }
+        
+        basic.showNumber(ans)
+    }
+    
+    if (mnmnumber == 2) {
+        index = randint(0, 3)
+        console.log("Index is :" + index)
+        basic.showString(messages[index])
+        Stage = 1
+    }
+    
+    if (mnmnumber == 1) {
+        ans = randint(0, 10)
+    }
+    
 })
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    if (operatorValue == "x") {
-        basic.showNumber(number1 * number12)
-    } else if (operatorValue == ":") {
-        basic.showNumber(number1 / number12)
-    } else if (operatorValue == "+") {
-        basic.showNumber(number1 + number12)
-    } else if (operatorValue == "-") {
-        basic.showNumber(number1 - number12)
-    } else {
-        basic.showString("Err")
+    if (mnmnumber == 3) {
+        if (operatorValue == "x") {
+            basic.showNumber(number1 * number12)
+        } else if (operatorValue == ":") {
+            basic.showNumber(number1 / number12)
+        } else if (operatorValue == "+") {
+            basic.showNumber(number1 + number12)
+        } else if (operatorValue == "-") {
+            basic.showNumber(number1 - number12)
+        } else {
+            basic.showString("Err")
+        }
+        
     }
     
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    console.log("Starting")
     
+    console.log(mnmnumber)
     if (mnmnumber < 3) {
         mnmnumber += 1
     } else {
@@ -49,15 +140,18 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     }
     
 })
-let mnmnumber = 0
-let Stage = 0
-let operatorValue = ""
-let operatorCounter = 0
-let opr : string[] = []
-let number12 = 0
-let number11 = 0
-let number1 = 0
-let number = 0
-basic.showString("Hi")
-basic.showString("Menu")
-let mnm = ["CA,G,C"]
+input.onButtonPressed(Button.A, function on_button_pressed_a3() {
+    console.log("Starting a2 :" + mnmnumber)
+    if (mnmnumber == 1) {
+        basic.showString("Game")
+    }
+    
+    if (mnmnumber == 2) {
+        basic.showString("Chat")
+    }
+    
+    if (mnmnumber == 3) {
+        basic.showString("Calculator")
+    }
+    
+})
